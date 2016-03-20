fd5459076e5033c4cef18c72d3481f712ce54988
Sidoine De Wispelaere <sidoine@sidoine.net>
2016-03-20 19:34:23 +0100

    Add ControllerTypeMerger

---
+++ src/Folke.Mvc.Extensions/ControllerTypeMerger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.AspNet.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Folke.Mvc.Extensions
+{
+    public class ControllerTypeMerger : IControllerTypeProvider
+    {
+        private readonly IServiceProvider serviceLocator;
+
+        public ControllerTypeMerger(IServiceProvider serviceLocator)
+        {
+            this.serviceLocator = serviceLocator;
+        }
+
+        public IEnumerable<TypeInfo> ControllerTypes
+        {
+            get
+            {
+                var providers = serviceLocator.GetServices<IControllerTypeProvider>();
+                foreach (var provider in providers)
+                {
+                    if (provider is ControllerTypeMerger) continue;
+
+                    foreach (var typeInfo in provider.ControllerTypes)
+                    {
+                        yield return typeInfo;
+                    }
+                }
+            }
+        }
+    }
+}
